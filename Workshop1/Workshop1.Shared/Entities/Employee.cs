b81deff328bcf491162191c535d5febc6bc6abd7
Isabel Rua <isabelrua19@gmail.com>
2025-09-08 23:27:35 -0500

    Basic project setup and added Employee entity

---
+++ Workshop1/Workshop1.Shared/Entities/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Workshop1.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    //Display para mostrar al usuario el nombre como uno quiere "decorado"
+    [Display(Name = "Nombre")]
+
+    //MaxLength para limitar la cantidad de caractéres que puede tener el campo
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres.")]
+
+    // Required para que el campo sea obligatorio
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string FirstName { get; set; } = null!; //Condición null! es para no permitir null en ese campo
+
+    [Display(Name = "Apellido")]
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "¿Esta activo?")]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha de Contratación")]
+    [DataType(DataType.DateTime)] // Indica que el campo debe mostrar fecha y hora
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [Range(1_000_000, double.MaxValue, ErrorMessage = "El salario debe ser al menos {1}.")]
+    public decimal Salary { get; set; }
+}
